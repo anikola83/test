73f4dd8367e4576b3fad180459948f1e9c7724a5
anikola83 <adzicnikola83@gmail.com>
2015-02-05 07:42:14 +0100

    Revert "new"
    This reverts commit f47bb9f787887e6bf28378ba3338298f3169c7ea.

--- template_footer_form.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo '<p id="error_message">'.$errorMessage.'</p>';
-echo '<script type="text/javascript" src="/validate.js"></script>
-</body></html>';
-?>
--- template_footer_users.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo '</table>';
-echo '<b id="error_message">'.$error_message.'</b>';
-echo '</body></html>';
-?>
